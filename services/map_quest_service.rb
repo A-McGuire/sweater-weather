a93c8e37321d1746ad56354b7635603bee40eead
Aidan McGuire <>
2021-06-10 15:55:33 -0600

    Add map_quest_service & figaro

---
+++ services/map_quest_service.rb
@@ -0,0 +1,23 @@
+class MapQuestService
+    class << self
+    def get_location_details(location)
+      resp = conn.get('address') do |req|
+        req.params['key'] = ENV['mapquest_key']
+        req.params['location'] = location
+      end
+      parse_data(resp)
+    end
+
+    private
+
+    def conn
+      Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/') do |faraday|
+        faraday.headers['Accept'] = '*/*'
+      end
+    end
+
+    def parse_data(resp)
+      JSON.parse(resp.body, symbolize_names: true)
+    end
+  end
+end
