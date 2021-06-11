c49f12b16a5bf3474b497ca1b0abffaf9f86af15
Aidan McGuire <>
2021-06-10 18:53:01 -0600

    Refactor open weather service

--- services/open_weather_service.rb
+++ services/open_weather_service.rb
@@ -1,10 +1,10 @@
 class OpenWeatherService
     class << self
-    def get_location_weather(lat, lon)
+    def get_location_weather(location)
       resp = conn.get('onecall') do |req|
         req.params['appid'] = ENV['open_weather_key']
-        req.params['lat'] = lat
-        req.params['lon'] = lon
+        req.params['lat'] = location[:lat]
+        req.params['lon'] = location[:lon]
       end
       parse_data(resp)
     end
