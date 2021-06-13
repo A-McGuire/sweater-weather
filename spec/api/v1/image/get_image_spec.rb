a82999b082a1a6e4588157fadc01605563157927
Aidan McGuire <>
2021-06-13 13:24:07 -0600

    Add get image spec

---
+++ spec/api/v1/image/get_image_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe '/backgrounds', :vcr do
+  it 'can get a locations image' do
+
+    get '/api/v1/forcast?location=denver, co'
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    
+    image = JSON.parse(response.body, symbolize_names: true)
+
+    expect(image).to be_a Hash
+    expect(image).to have_key(:data)
+    expect(image[:data]).to be_a Hash
+    expect(image[:data].keys).to eq([:id, :type, :attributes])
+    expect(image[:data][:id]).to eq(nil)
+    expect(image[:data][:type]).to eq('image')
+    expect(image[:data][:attributes].keys).to eq([:image])
+    expect(image[:data][:attributes][:image].keys).to eq([:location, :image_url, :credit])
+    expect(image[:data][:attributes][:image]).to be_a Hash
+    expect(image[:data][:attributes][:image][:credit].keys).to eq([:source, :author, :logo])
+  end
+end
