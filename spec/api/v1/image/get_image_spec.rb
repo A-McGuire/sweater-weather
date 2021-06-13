7f352d1440afa741a2abb88445c0b6876a8e8517
Aidan McGuire <>
2021-06-13 14:00:32 -0600

    Change expected image endpoint data

--- spec/api/v1/image/get_image_spec.rb
+++ spec/api/v1/image/get_image_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe '/backgrounds', :vcr do
   it 'can get a locations image' do
 
-    get '/api/v1/forcast?location=denver, co'
+    get '/api/v1/backgrounds?location=denver, co'
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
@@ -17,14 +17,17 @@ RSpec.describe '/backgrounds', :vcr do
     expect(image[:data][:id]).to eq(nil)
     expect(image[:data][:type]).to eq('image')
     expect(image[:data][:attributes].keys).to eq([:image])
-    expect(image[:data][:attributes][:image].keys).to eq([:location, :image_url, :credit])
-    expect(image[:data][:attributes][:location]).to be_a String
-    expect(image[:data][:attributes][:image_url]).to be_a String
-    expect(image[:data][:attributes][:credit]).to be_a String
     expect(image[:data][:attributes][:image]).to be_a Hash
-    expect(image[:data][:attributes][:image][:credit].keys).to eq([:source, :author, :logo])
+    expect(image[:data][:attributes][:image].keys).to eq([:location, :image_url_full, :image_url_regular, :image_url_small, :image_url_thumb :credit])
+    expect(image[:data][:attributes][:location]).to be_a String
+    expect(image[:data][:attributes][:image_url_full]).to be_a String
+    expect(image[:data][:attributes][:image_url_regular]).to be_a String
+    expect(image[:data][:attributes][:image_url_small]).to be_a String
+    expect(image[:data][:attributes][:image_url_thumb]).to be_a String
+    expect(image[:data][:attributes][:credit]).to be_a String
+    expect(image[:data][:attributes][:image][:credit].keys).to eq([:source, :author, :author_profile])
     expect(image[:data][:attributes][:image][:credit][:source]).to be_a String
     expect(image[:data][:attributes][:image][:credit][:author]).to be_a String
-    expect(image[:data][:attributes][:image][:credit][:logo]).to be_a String
+    expect(image[:data][:attributes][:image][:credit][:author_profile]).to be_a String
   end
 end
