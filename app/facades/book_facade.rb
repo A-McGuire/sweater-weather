0d86c5cfba8cb73decd2d6f4268e5ae40c65d644
Aidan McGuire <>
2021-06-14 10:17:32 -0600

    Change book facade var duplicate name

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -2,19 +2,19 @@ class BookFacade
   class << self
     def location_book_data(location, quantity)
       forecast = ForecastFacade.location_weather_data(location)
-      books = OpenLibraryService.get_location_books(location, quantity)
+      data = OpenLibraryService.get_location_books(location, quantity)
       books = {
         destination: location,
         forecast: {
           summary: forecast.current_weather[:conditions],
           temperature: "#{forecast.current_weather[:temperature]} F"
         },
-        total_books_found: books[:numFound],
-        books: books[:docs].map do |book|
+        total_books_found: data[:numFound],
+        books: data[:docs].map do |book|
           {
-            isbn: book[:isbn],
-            title: book[:title],
-            publisher: book[:publisher]
+            isbn: data[:isbn],
+            title: data[:title],
+            publisher: data[:publisher]
           }
         end
       }
