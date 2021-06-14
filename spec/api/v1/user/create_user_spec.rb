dea77c600c37e88c020605b905553ca6e284c827
Aidan McGuire <>
2021-06-14 13:47:48 -0600

    Add sad path handling for post /users

--- spec/api/v1/user/create_user_spec.rb
+++ spec/api/v1/user/create_user_spec.rb
@@ -31,5 +31,105 @@ RSpec.describe 'POST /users' do
     expect(user.email).to eq('whatever@example.com')
   end
 
-  it 'responds with a 400 level status with description why in body if failed'
+  describe 'sad path' do
+    it 'responds with a 409 level status if email is taken' do
+      user = User.create!(email: 'whatever@example.com', password: '123', password_confirmation: '123')
+      body = {
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+  
+      post '/api/v1/users', params: body.to_json, headers: { "Content-Type": "application/json" }
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(409)
+  
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq('Email has already been taken')
+    end
+  
+    it 'responds with a 400 level status if email is blank' do
+      body = {
+        email: "",
+        password: "123",
+        password_confirmation: "123"
+      }
+  
+      post '/api/v1/users', params: body.to_json, headers: { "Content-Type": "application/json" }
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq("Email is required")
+    end
+  
+    it 'responds with a 400 level status if email is missing' do
+      body = {
+        password: "123",
+        password_confirmation: "123"
+      }
+  
+      post '/api/v1/users', params: body.to_json, headers: { "Content-Type": "application/json" }
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq("Email is required")
+    end
+  
+    it 'responds with a 400 level status if passwords do not match' do
+      body = {
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "pass"
+      }
+  
+      post '/api/v1/users', params: body.to_json, headers: { "Content-Type": "application/json" }
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq("Passwords do not match")
+    end
+  
+    it 'responds with a 400 level status if password is blank' do
+      body = {
+        email: "whatever@example.com",
+        password: "",
+        password_confirmation: ""
+      }
+  
+      post '/api/v1/users', params: body.to_json, headers: { "Content-Type": "application/json" }
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq("Password is required")
+    end
+
+    it 'responds with a 400 level status if password is missing' do
+      body = {
+        email: "whatever@example.com"
+      }
+  
+      post '/api/v1/users', params: body.to_json, headers: { "Content-Type": "application/json" }
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq("Password is required")
+    end
+  end
 end
