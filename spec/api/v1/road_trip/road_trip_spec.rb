85abf2093f0a493b4c2dbb919fdf0dbf8bb32f06
Aidan McGuire <>
2021-06-14 17:53:27 -0600

    Add road trip spec

---
+++ spec/api/v1/road_trip/road_trip_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /users', :vcr do
+  it 'returns the correct json structure' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    body = {
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "auth_token": "#{user.auth_token}"
+    }
+
+    post '/api/v1/road_trip', params: body.to_json, headers: { "Content-Type": "application/json", "Accept": "application/json" }
+
+    user = User.find_by(email: 'whatever@example.com')
+    
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    resp = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(resp.keys).to eq([:data])
+    expect(resp[:data]).to be_a Hash
+    expect(resp[:data].keys).to eq([:id, :type, :attributes])
+    expect(resp[:data][:type]).to eq('roadtrip')
+    expect(resp[:data][:id]).to eq(nil)
+    expect(resp[:data][:attributes]).to be_a Hash
+    expect(resp[:data][:attributes].keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
+    expect(resp[:data][:attributes][:start_city]).to be_a String
+    expect(resp[:data][:attributes][:end_city]).to be_a String
+    expect(resp[:data][:attributes][:travel_time]).to be_a String
+    expect(resp[:data][:attributes][:weather_at_eta]).to be_a Hash
+    expect(resp[:data][:attributes][:weather_at_eta].keys).to eq([:temperature, :conditions])
+  end
+end
