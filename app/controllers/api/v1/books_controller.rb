eb5632e085a9d3088ca9f9e2be7536f0ef124c1a
Aidan McGuire <>
2021-06-14 11:34:53 -0600

    Format books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,17 +1,11 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    if params[:location].nil? || params[:location] == ''
-      return render json: { error: 'location parameter required' },
-                    status: :bad_request
-    end
-    if params[:quantity].nil? || params[:quantity] == ''
-      return render json: { error: 'quantity parameter required' },
-                    status: :bad_request
-    end
-    if params[:quantity].to_i <= 0
-      return render json: { error: 'quantity parameter must be an integer greater than 0' },
-                    status: :bad_request
-    end
+    return render json: {error: 'location parameter required'}, 
+      status: :bad_request if params[:location].nil? || params[:location] == ''
+    return render json: {error: 'quantity parameter required'}, 
+      status: :bad_request if params[:quantity].nil? || params[:quantity] == ''
+    return render json: {error: 'quantity parameter must be an integer greater than 0'}, 
+      status: :bad_request if params[:quantity].to_i <= 0
     books = BookFacade.location_book_data(params[:location], params[:quantity])
     render json: BookSerializer.new(books).serializable_hash
   end
