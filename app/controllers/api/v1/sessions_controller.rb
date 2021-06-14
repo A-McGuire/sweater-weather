7d40bf0cc242b54388d08993928b2750cc9af0cd
Aidan McGuire <>
2021-06-14 16:13:38 -0600

    Add user login sad path

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       render json: UsersSerializer.new(user), status: 200
+    else
+      render json: {errors: 'Invalid user credentials'}, status: 400
     end
   end
 end
--- spec/api/v1/user/user_login_spec.rb
+++ spec/api/v1/user/user_login_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'POST /sessions' do
+RSpec.describe 'POST /sessions', :vcr do
   it 'recieves user data as json in body and logs user in' do
     user = User.create!(email: "whatever@example.com", password: "password")
 
@@ -7,7 +7,7 @@ RSpec.describe 'POST /sessions' do
       password: "password"
     }
 
-    post '/api/v1/sessions', params: body.to_json, headers: { "Content-Type": "application/json" }
+    post '/api/v1/sessions', params: body.to_json, headers: { "Content-Type": "application/json", "Accept": "application/json"}
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
@@ -23,5 +23,76 @@ RSpec.describe 'POST /sessions' do
     expect(resp[:data][:attributes].keys).to eq([:email, :auth_token])
     expect(resp[:data][:attributes][:email]).to eq(user.email)
     expect(resp[:data][:attributes][:auth_token]).to eq(user.auth_token)
+  end
+
+  describe 'sad path' do
+    it 'responds with a 400 level status if email is incorrect' do
+      user = User.create!(email: 'whatever@example.com', password: 'password')
+      body = {
+        email: "what@example.com",
+        password: "password",
+      }
+  
+      post '/api/v1/sessions', params: body.to_json, headers: { "Content-Type": "application/json", "Accept": "application/json"}
+      
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+  
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq('Invalid user credentials')
+    end
+
+    it 'responds with a 400 level status if email is blank' do
+      user = User.create!(email: 'whatever@example.com', password: 'password')
+      body = {
+        email: "",
+        password: "password",
+      }
+  
+      post '/api/v1/sessions', params: body.to_json, headers: { "Content-Type": "application/json", "Accept": "application/json"}
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+  
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq('Invalid user credentials')
+    end
+
+    it 'responds with a 400 level status if password is incorrect' do
+      user = User.create!(email: 'whatever@example.com', password: 'password')
+      body = {
+        email: "whatever@example.com",
+        password: "pass",
+      }
+  
+      post '/api/v1/sessions', params: body.to_json, headers: { "Content-Type": "application/json", "Accept": "application/json"}
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+  
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq('Invalid user credentials')
+    end
+
+    it 'responds with a 400 level status if password is blank' do
+      user = User.create!(email: 'whatever@example.com', password: 'password')
+      body = {
+        email: "whatever@example.com",
+        password: "",
+      }
+  
+      post '/api/v1/sessions', params: body.to_json, headers: { "Content-Type": "application/json", "Accept": "application/json"}
+      resp = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+  
+      expect(resp.keys).to eq([:errors])
+      expect(resp[:errors]).to eq('Invalid user credentials')
+    end
   end
 end
