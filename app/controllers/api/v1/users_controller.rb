d3292bcda8851afb3518a5ec70d631fabf75a519
Aidan McGuire <>
2021-06-14 14:12:08 -0600

    Remove errorable

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  include Errorable
 
   def create
     user = user_params
@@ -17,5 +16,12 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def registration_errors(new_user)
+    return render json: {errors: 'Email has already been taken'}, status: 409 if new_user.errors.messages[:email] == ["has already been taken"]
+    return render json: {errors: 'Email is required'}, status: 400 if new_user.errors.messages[:email] == ["can't be blank"]
+    return render json: {errors: 'Passwords do not match'}, status: 400 if new_user.errors.messages[:password_confirmation] == ["doesn't match Password"]
+    return render json: {errors: 'Password is required'}, status: 400 if new_user.errors.messages[:password] == ["can't be blank"]
   end
 end
--- app/modules/errorable.rb
+++
@@ -1,8 +0,0 @@
-module Errorable
-  def registration_errors(new_user)
-    return render json: {errors: 'Email has already been taken'}, status: 409 if new_user.errors.messages[:email] == ["has already been taken"]
-    return render json: {errors: 'Email is required'}, status: 400 if new_user.errors.messages[:email] == ["can't be blank"]
-    return render json: {errors: 'Passwords do not match'}, status: 400 if new_user.errors.messages[:password_confirmation] == ["doesn't match Password"]
-    return render json: {errors: 'Password is required'}, status: 400 if new_user.errors.messages[:password] == ["can't be blank"]
-  end
-end
