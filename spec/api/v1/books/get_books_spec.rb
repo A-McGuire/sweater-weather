80a69f1f9b5e299c6ce7ec5d8bc1dd6cf733e74f
Aidan McGuire <>
2021-06-14 09:16:23 -0600

    Add get books spec

---
+++ spec/api/v1/books/get_books_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe '/book-search', :vcr do
+  it 'can get a locations books, and forcast' do
+
+    get '/api/v1/book-search?location=denver,co&quantity=5'
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(data).to be_a Hash
+    expect(data).to have_key(:data)
+    expect(data[:data]).to be_a Hash
+    expect(data[:data].keys).to eq([:id, :type, :attributes])
+    expect(data[:data][:id]).to eq(nil)
+    expect(data[:data][:type]).to eq('books')
+    expect(data[:data][:attributes].keys).to eq([:destination, :forcast, :total_books_found, :books])
+    expect(data[:data][:attributes][:destination]).to be_a String
+
+    expect(data[:data][:attributes][:forcast]).to be_a Hash
+    expect(data[:data][:attributes][:forcast].keys).to eq([:summary, :temperature])
+    expect(data[:data][:attributes][:forcast][:summary]).to be_a String
+    expect(data[:data][:attributes][:forcast][:temperature]).to be_a Float
+
+    expect(data[:data][:attributes][:total_books_found]).to be_a Integer
+    expect(data[:data][:attributes][:books]).to be_an Array
+    expect(data[:data][:attributes][:books].count).to eq(5)
+    expect(data[:data][:attributes][:books].first).to be_a Hash
+    expect(data[:data][:attributes][:books].first.keys).to eq([:isbn, :title, :publisher])
+    expect(data[:data][:attributes][:books].first[:isbn]).to be_an Array
+    expect(data[:data][:attributes][:books].first[:title]).to be_an String
+    expect(data[:data][:attributes][:books].first[:publisher]).to be_an String
+  end
+end
