b465f0846343ffbd5b316814bc68af8fff66c809
Aidan McGuire <>
2021-06-14 11:31:39 -0600

    Appease rubocop

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    return render json: {error: 'location parameter required'}, 
-      status: :bad_request if params[:location].nil? || params[:location] == ''
-    return render json: {error: 'quantity parameter required'}, 
-      status: :bad_request if params[:quantity].nil? || params[:quantity] == ''
-    return render json: {error: 'quantity parameter must be an integer greater than 0'}, 
-      status: :bad_request if params[:quantity].to_i <= 0
+    if params[:location].nil? || params[:location] == ''
+      return render json: { error: 'location parameter required' },
+                    status: :bad_request
+    end
+    if params[:quantity].nil? || params[:quantity] == ''
+      return render json: { error: 'quantity parameter required' },
+                    status: :bad_request
+    end
+    if params[:quantity].to_i <= 0
+      return render json: { error: 'quantity parameter must be an integer greater than 0' },
+                    status: :bad_request
+    end
     books = BookFacade.location_book_data(params[:location], params[:quantity])
     render json: BookSerializer.new(books).serializable_hash
   end
--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::WeatherController < ApplicationController
   def forecast
-    return render status: 400 if params[:location].nil? || params[:location] == ''
+    return render status: :bad_request if params[:location].nil? || params[:location] == ''
+
     forecast = ForecastFacade.location_weather_data(params[:location])
     render json: ForecastSerializer.new(forecast).serializable_hash
   end
