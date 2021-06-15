074894725489e047dfae77580f70156867b9e33a
Aidan McGuire <>
2021-06-14 19:30:14 -0600

    Implement api key verification for /road_trip

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RoadTripController < ApplicationController
   def index
+    return render status: 401 if User.find_by(auth_token: params[:auth_token]).nil? || params[:auth_token].nil?
     trip_details = RoadTripFacade.get_trip_details({origin: params[:origin], destination: params[:destination]})
     render json: RoadTripSerializer.new(trip_details).serializable_hash
   end
