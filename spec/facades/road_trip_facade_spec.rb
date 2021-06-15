256584f8210c8c3859847c65003f7ff6be113fdc
Aidan McGuire <>
2021-06-15 10:50:30 -0600

    Add road trip facade spec

---
+++ spec/facades/road_trip_facade_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'road trip facade' do
+  describe 'get_trip_details', :vcr do 
+    it 'creates an object with the correct attributes' do
+      params = { origin: "Denver, CO", destination: "Estes Park, CO" }
+      data = RoadTripFacade.get_trip_details(params)
+      
+      expect(data.id).to eq(nil)
+      expect(data.start_city).to be_a String
+      expect(data.end_city).to be_a String
+      expect(data.travel_time).to be_a String
+      expect(data.weather_at_eta).to be_a Hash
+      expect(data.weather_at_eta.keys).to eq([:temperature, :conditions])
+    end
+  end
+
+  describe 'travel_time_to_nearest_hour helper method' do
+    it 'recieves seconds and returns an integer rounded to the nearest hour' do
+      expect(RoadTripFacade.travel_time_to_nearest_hour(3600)).to eq(1)
+      expect(RoadTripFacade.travel_time_to_nearest_hour(3000)).to eq(1)
+      expect(RoadTripFacade.travel_time_to_nearest_hour(7000)).to eq(2)
+      expect(RoadTripFacade.travel_time_to_nearest_hour(7400)).to eq(2)
+      expect(RoadTripFacade.travel_time_to_nearest_hour(172800)).to eq(48)
+    end
+  end
+end
