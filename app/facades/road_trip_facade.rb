f8c7485f40de60992330ffe1563265218bb9373b
Aidan McGuire <>
2021-06-15 10:39:06 -0600

    Refactor road trip facade helper

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,22 +1,23 @@
 class RoadTripFacade
   class << self
+    def travel_time_to_nearest_hour(seconds)
+      minutes = seconds / 60.0
+      hour = (minutes / 60).round
+    end
+
     def get_trip_details(params)
       destination_forcast = ForcastFacade.location_weather_data(params[:destination])
       @directions = MapQuestService.get_directions(params)
       
-      def travel_time_to_nearest_hour
-        minutes = @directions[:route][:realTime] / 60.0
-        hour = (minutes / 60).round
-      end
       
-      forcast = ForcastFacade.location_weather_data(params[:destination], travel_time_to_nearest_hour)
+      forcast = ForcastFacade.location_weather_data(params[:destination], self.travel_time_to_nearest_hour(@directions[:route][:realTime]))
       
-      start_city = params[:origin]
+      start_city = params[:origin]  
       end_city = params[:destination]
       travel_time = @directions[:route][:formattedTime]
       weather_at_eta = {
-          temperature: forcast[:hourly_weather][travel_time_to_nearest_hour - 1][:temperature],
-          conditions: forcast[:hourly_weather][travel_time_to_nearest_hour - 1][:conditions]
+          temperature: forcast[:hourly_weather][self.travel_time_to_nearest_hour(@directions[:route][:realTime]) - 1][:temperature],
+          conditions: forcast[:hourly_weather][self.travel_time_to_nearest_hour(@directions[:route][:realTime]) - 1][:conditions]
         }
 
       OpenStruct.new(
