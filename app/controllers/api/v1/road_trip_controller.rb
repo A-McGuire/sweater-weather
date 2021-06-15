9955824ad61998cd1078a96ae145614c5fe573ac
Aidan McGuire <>
2021-06-14 19:38:36 -0600

    Fix raod trip error handling conditions

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RoadTripController < ApplicationController
   def index
-    return render status: 401 if User.find_by(auth_token: params[:auth_token]).nil? || params[:auth_token].nil?
+    return render status: 401 if User.find_by(auth_token: params[:api_key]).nil? || params[:api_key].nil?
     trip_details = RoadTripFacade.get_trip_details({origin: params[:origin], destination: params[:destination]})
     render json: RoadTripSerializer.new(trip_details).serializable_hash
   end
